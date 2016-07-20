59a82c78132df0faa297e581fe6f82272a43b3ee
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2016-07-20 05:34:22 +0300

    Added to bintray + update clock method

--- app/src/main/java/com/tomerrosenfeld/analogclockview/MainActivity.java
+++ app/src/main/java/com/tomerrosenfeld/analogclockview/MainActivity.java
@@ -1,9 +1,12 @@
 package com.tomerrosenfeld.analogclockview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 import com.tomerrosenfeld.customanalogclockview.CustomAnalogClock;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         CustomAnalogClock customAnalogClock = (CustomAnalogClock) findViewById(R.id.analog_clock);
+        updateClock(customAnalogClock);
+    }
+
+    private void updateClock(final CustomAnalogClock customAnalogClockView) {
+        customAnalogClockView.setTime(Calendar.getInstance());
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                updateClock(customAnalogClockView);
+            }
+        }, 5000);
     }
 }
